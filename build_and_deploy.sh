933fc296d6815ddcae9dee7e10eaf702f35d7af0
Daniel Moon <daniel_moon16@yahoo.com>
2024-04-16 15:17:44 -0400

    turns out I already had that funcitonality elsewhere

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -20,17 +20,11 @@ ssh "$PI_USER@$PI_IP" "docker stop $DOCKER_TAG " || true
 echo "Removing old container"
 ssh "$PI_USER@$PI_IP" "docker container rm $DOCKER_TAG " || true
 
-echo "Grabbing current image id"
-OLD_IMAGE=$(ssh "$PI_USER@$PI_IP" "docker images --filter=reference='$DOCKER_TAG' --format '{{.ID}}'")
-echo $OLD_IMAGE
-
 echo "Pushing new image"
 docker save $DOCKER_TAG | bzip2 | ssh -l $PI_USER $PI_IP docker load
 
 echo "Starting Container"
 ssh "$PI_USER@$PI_IP" "docker run -d -e POSTGRES_DB=$POSTGRES_DB -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD --network host -v /var/lib/postgres:/var/lib/postgresql/data --restart unless-stopped --name $DOCKER_TAG \"$DOCKER_TAG\""
 
-if [ -n "$OLD_IMAGE" ]; then
-  echo "Removing old image"
-  ssh "$PI_USER@$PI_IP" "docker image rm $OLD_IMAGE " || true
-fi
+echo "Removing dangling images"
+ssh "$PI_USER@$PI_IP" 'docker image rm $(docker images -f "dangling=true" -q)'
