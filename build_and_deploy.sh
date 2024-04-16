3c3de3bf3eee5b1ed069a45a3c39147b6a4d0602
Daniel Moon <daniel_moon16@yahoo.com>
2024-04-16 13:47:13 -0400

    storing data in a volume,  updating build script to clean up afterwards

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -5,6 +5,9 @@ set -e
 # Set script vars
 DOCKER_TAG="postgres"
 
+echo "Setting permissions"
+ssh "$PI_USER@$PI_IP" "sudo chown 70:70 /var/lib/postgres"
+
 echo "Building target for arm64"
 docker buildx build --platform linux/arm64 -t $DOCKER_TAG .
 
@@ -14,11 +17,15 @@ ssh "$PI_USER@$PI_IP" "docker stop $DOCKER_TAG " || true
 echo "Removing old container"
 ssh "$PI_USER@$PI_IP" "docker container rm $DOCKER_TAG " || true
 
-# echo "Removing old image"
-# ssh "$PI_USER@$PI_IP" "docker image rm $DOCKER_TAG " || true
+echo "Grabbing current image id"
+OLD_IMAGE=$(ssh "pi@192.168.1.7" "docker images --filter=reference='postgres' --format '{{.ID}}'")
+echo $OLD_IMAGE
 
 echo "Pushing new image"
 docker save $DOCKER_TAG | bzip2 | ssh -l $PI_USER $PI_IP docker load
 
 echo "Starting Container"
-ssh "$PI_USER@$PI_IP" "docker run -d --network host --restart unless-stopped --name $DOCKER_TAG \"$DOCKER_TAG\""
+ssh "$PI_USER@$PI_IP" "docker run -d --network host -v /var/lib/postgres:/var/lib/postgresql/data --restart unless-stopped --name $DOCKER_TAG \"$DOCKER_TAG\""
+
+echo "Removing old image"
+ssh "$PI_USER@$PI_IP" "docker image rm $OLD_IMAGE " || true
