1e33378e2813223ba4ce45e832e6b3338b0b08ce
Daniel Moon <daniel_moon16@yahoo.com>
2024-09-19 13:42:42 -0400

    updating script to allow easy host switching for deploys

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -6,10 +6,11 @@ source ./env
 
 # Set script vars
 DOCKER_TAG="postgres"
+HOST=$DB_IP
 
 echo "Setting permissions"
-ssh "$PI_USER@$PI_IP" "sudo mkdir /var/lib/postgres" || true
-ssh "$PI_USER@$PI_IP" "sudo chown 70:70 /var/lib/postgres"
+ssh "$PI_USER@$HOST" "sudo mkdir /var/lib/postgres" || true
+ssh "$PI_USER@$HOST" "sudo chown 70:70 /var/lib/postgres"
 
 echo "Setting builder to default"
 docker buildx use default
@@ -18,16 +19,16 @@ echo "Building target for arm64"
 docker buildx build --platform linux/arm64 -t $DOCKER_TAG . --load
 
 echo "Stopping old container"
-ssh "$PI_USER@$PI_IP" "docker stop $DOCKER_TAG " || true
+ssh "$PI_USER@$HOST" "docker stop $DOCKER_TAG " || true
 
 echo "Removing old container"
-ssh "$PI_USER@$PI_IP" "docker container rm $DOCKER_TAG " || true
+ssh "$PI_USER@$HOST" "docker container rm $DOCKER_TAG " || true
 
 echo "Pushing new image"
-docker save $DOCKER_TAG | bzip2 | ssh -l $PI_USER $PI_IP docker load
+docker save $DOCKER_TAG | bzip2 | ssh -l $PI_USER $HOST docker load
 
 echo "Starting Container"
-ssh "$PI_USER@$PI_IP" "docker run -d -e POSTGRES_DB=$POSTGRES_DB -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD --network host -v /var/lib/postgres:/var/lib/postgresql/data --restart unless-stopped --name $DOCKER_TAG \"$DOCKER_TAG\""
+ssh "$PI_USER@$HOST" "docker run -d -e POSTGRES_DB=$POSTGRES_DB -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD --network host -v /var/lib/postgres:/var/lib/postgresql/data --restart unless-stopped --name $DOCKER_TAG \"$DOCKER_TAG\""
 
 echo "Removing dangling images"
-ssh "$PI_USER@$PI_IP" 'docker image rm $(docker images -f "dangling=true" -q)'
+ssh "$PI_USER@$HOST" 'docker image rm $(docker images -f "dangling=true" -q)'
--- scripts/nuke_db.sh
+++ scripts/nuke_db.sh
@@ -3,12 +3,13 @@
 set -e
 
 echo "Are you sure? (y/n)"
+HOST=$DB_IP
 
 read -r line
 
 if [ "$line" = "y" ]; then
-    ssh "$PI_USER@$PI_IP" "sudo chown -R $PI_USER:$PI_USER /var/lib/postgres"
-    ssh "$PI_USER@$PI_IP" "sudo rm -R /var/lib/postgres"
+    ssh "$PI_USER@$HOST" "sudo chown -R $PI_USER:$PI_USER /var/lib/postgres"
+    ssh "$PI_USER@$HOST" "sudo rm -R /var/lib/postgres"
 fi
 
 
